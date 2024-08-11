8856594356eb3d1857f9fd1f06a7b0744ccca8c4
SANTHANAKRISHNANALWAR <140005781+SANTHANAKRISHNANALWAR@users.noreply.github.com>
2024-08-12 00:57:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+function image2(){
+    window.open('https://www.goodfirms.co/directory/platform/app-development')
+}
+
+function facebook() {
+    window.open('https://www.facebook.com/ieyalsolutions/');
+}
+
+function instagram() {
+    window.open('https://www.instagram.com/ieyalsolutions/')
+}
+
+function linkedin(){
+    window.open('https://in.linkedin.com/company/ieyalsolutions')
+}
+
+function pinterest(){
+    window.open('https://in.pinterest.com/ieyalsolutions/')
+}
+
+function twitter(){
+    window.open('https://x.com/i/flow/login?redirect_after_login=%2FIeyalSolutions')
+}
+
+function youtube(){
+    window.open('https://www.youtube.com/channel/UCuKkUzbea-tU0Nm525McvsQ')
+}
+
+function mail(){
+    window.open('mailto:sales@ieyalsolutions.com')
+}
+
+function rss(){
+    window.open('https://blog.ieyalsolutions.com/')
+}
+
+function phone(){
+    location.href=('tel:+919791467587')
+}
+
+function gps(){
+    // window.location.href='https://www.google.com/maps/place/Ieyal+Solutions/@10.7698703,79.6362214,17z/data=!4m6!3m5!1s0x3a55470ec2badbc3:0x18433afde9b68c06!8m2!3d10.769865!4d79.6384101!16s%2Fg%2F11jdrzmxpm?entry=ttu';
+    window.open('https://www.google.com/maps/place/Ieyal+Solutions/@10.7698703,79.6362214,17z/data=!4m6!3m5!1s0x3a55470ec2badbc3:0x18433afde9b68c06!8m2!3d10.769865!4d79.6384101!16s%2Fg%2F11jdrzmxpm?entry=ttu')
+}
